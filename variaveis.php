8a7e57c36c59bd0d29513193307849a36854f92a
Danilo Dalonso Gamboa Peres <danilodgperes@yahoo.com.br>
2018-11-05 19:55:58 -0200

    variaveis

---
+++ variaveis.php
@@ -0,0 +1,14 @@
+<?php
+    $nome= "Danilo";
+    $sobrenome= "Dalonso";
+    $endereco= "São Paulo - SP";
+    $idade= 34;
+    $telefone= 555.9999;
+
+    echo "Meu nome é ";
+    echo "$nome $sobrenome";
+    echo "<br> Meu endereço é $endereco";
+    echo "<br> Tenho $idade anos";
+    echo "<br> Meu telefone é $telefone";
+
+?>
