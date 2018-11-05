9b87e53b72137cccc50a82f6df664ae0445a2712
Danilo Dalonso Gamboa Peres <danilodgperes@yahoo.com.br>
2018-11-05 20:46:49 -0200

    ExercicioAula03

---
+++ maioridade.php
@@ -0,0 +1,12 @@
+<?php
+    $nome=Danilo;
+    $idade=34;
+
+    echo "Olá $nome";
+
+    if($idade >= 21){
+        echo "<br>Você já tem maioridade jurídica.";
+    } else{
+        echo"<br>Você ainda não tem maioridade jurídica.";
+    }
+    ?>
