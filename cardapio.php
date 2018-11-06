9493cda6db6a79a1bfe0cdf92169b2c2a0970fc4
Danilo Dalonso Gamboa Peres <danilodgperes@yahoo.com.br>
2018-11-06 16:00:01 -0200

    ExercicioAula08

---
+++ cardapio.php
@@ -0,0 +1,33 @@
+<?php
+    
+    $opcao = 1;
+
+    echo "A escolha do cliente é:<br><br>";
+    
+    switch($opcao){
+
+        case(1):
+            echo "Arroz, feijão, bife e ovo";
+            break;
+        case(2):
+            echo "Arroz, feijão, filé de frango e salada";
+            break;
+        case(3):
+            echo "Macarrão com frango assado";
+            break;
+        case(4):
+            echo "Filé de peixe, arroz à grega e purê";
+            break;
+        case(5):
+            echo "Lasanha Vegetariana";
+            break;
+        case(6):
+            echo "Strogonoff com arroz";
+            break;
+
+
+
+    }
+
+
+?>
