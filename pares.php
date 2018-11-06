07ec435e01fa65c7c0d4ea342c4b6c270d5af66f
Danilo Dalonso Gamboa Peres <danilodgperes@yahoo.com.br>
2018-11-06 17:18:42 -0200

    ExercicioAula07

--- pares.php
+++ pares.php
@@ -5,15 +5,17 @@
     $divisor=2;
 
     $contador = $numInicial;
-    
-    echo "Entre " . $numInicial . " e " . $numFinal . " apenas os números abaixo são pares:<br><br>";
+         
+        echo "Entre " . $numInicial . " e " . $numFinal . " somente os números abaixo são pares:<br><br>";
+
         while($contador <= $numFinal){
-        $resto = $contador % $divisor;
-        if($resto == 0){
-            echo  $contador . "<br>";
-        }   
-        $contador += 1;
-    }
+            $resto = $contador % $divisor;
+            if($resto == 0){
+            echo $contador . "<br>";
+        }
+            $contador += 1;
+        }
+    
    
 
 
