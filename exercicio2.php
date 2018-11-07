7bcfd48618d1ce5875a3a33512d0b3ff127e2e1d
Danilo Dalonso Gamboa Peres <danilodgperes@yahoo.com.br>
2018-11-07 14:21:52 -0200

    Exercicio2

--- exercicio2.php
+++ exercicio2.php
@@ -4,22 +4,25 @@ $numero = 10;
 $div10 = 10;
 $div5 = 5;
 $div2 = 2;
+$opcao = 1
 
-
-
-
-if($numero % $div10 == 0):
-    echo "O número " . $numero . " é divisível por 10<br>";
+switch($opcao){
+    case(1):
+    $numero % $div10 == 0; 
+    echo "O número " . $numero . " é divisível por 10";
     
-elseif($numero % $div5 == 0):
-    echo "O número " . $numero . " é divisível por 5<br>";
+    case(2):
+    $numero % $div5 == 0;
+    echo "O número " . $numero . " é divisível por 5";
 
-elseif($numero % $div2 ==0):
-        echo "O número " . $numero . " é divisível por 2<br>";
+    case(3):
+    $numero % $div2 == 0;
+    echo "O número " . $numero . " é divisivel por 2";
 
-else:
-    echo "O número " . $numero . " não é divisível por 10, por 5 e nem por 2.<br>";
+    default:
+        echo "O número " . $numero . " não é divisível por 10, por 5 nem por 2.";
+        break;
 
-endif;
+}
 
 ?>
