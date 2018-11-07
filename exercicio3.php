7d7265fcacd478f9d46af23d70f32f57528a7fb1
Danilo Dalonso Gamboa Peres <danilodgperes@yahoo.com.br>
2018-11-07 15:38:46 -0200

    Exercicio3

---
+++ exercicio3.php
@@ -0,0 +1,15 @@
+<?php
+$nome = Juniana;
+$sexo = feminino;
+$idade = 20;
+
+if($sexo == feminino && $idade < 25){
+    echo $nome . "<br>";
+    echo "ACEITA";
+}else{
+    echo $nome . "<br>";
+    echo "NÃO ACEITA";
+}
+
+
+?>
