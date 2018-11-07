9e5589822e6ca8c72e1a7c15a1bcc1dd5950a3ec
Danilo Dalonso Gamboa Peres <danilodgperes@yahoo.com.br>
2018-11-07 17:45:37 -0200

    AulaListas

---
+++ funcionarios.php
@@ -0,0 +1,8 @@
+<?php
+
+$funcionarios = array ("Danilo", "Leonardo", "Nathaly", "Vinicius" , "Andre", "Junior", "Rodrigo", "Wendel", "Pablo", "Ane", "Fran", "Marcus");
+
+print("<pre>");
+var_dump($funcionarios);
+
+?>
