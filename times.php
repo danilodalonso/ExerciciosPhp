50bef60b9b5eb26111dd32d3904bc4852a551f0f
Danilo Dalonso Gamboa Peres <danilodgperes@yahoo.com.br>
2018-11-08 22:03:28 -0200

    AulaForeach

---
+++ times.php
@@ -0,0 +1,30 @@
+<?php
+
+$times = array (
+    "América Mineiro",
+    "Atlético Mineiro",
+    "Atlético Paranaense",
+    "Bahia",
+    "Botafogo",
+    "Ceará",
+    "Chapecoense",
+    "Corinthians",
+    "Cruzeiro",
+    "Flamengo",
+    "Fluminense",
+    "Grêmio",
+    "Internacional",
+    "Palmeiras",
+    "Paraná",
+    "Santos",
+    "São Paulo",
+    "Sport",
+    "Vasco da Gama",
+    "Vitória"
+
+);
+
+foreach($times as $time){
+    echo $time . "<br>";
+}
+?>
