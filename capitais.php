2f26ee4aca507ffc7c7990e3eb0c0d3da02ea5f4
Danilo Dalonso Gamboa Peres <danilodgperes@yahoo.com.br>
2018-11-09 12:43:21 -0200

    Array Next

---
+++ capitais.php
@@ -0,0 +1,37 @@
+<?php
+
+$capitais = array(
+    "São Paulo",
+    "Rio de Janeiro",
+    "Belo Horizonte",
+    "Vitória",
+    "Curitiba",
+    "Paraná",
+    "Rio Grande do Sul",
+    "Rio Branco",
+    "Manaus",
+    "Palmas",
+);
+
+echo current($capitais) . "<br>";
+
+echo next($capitais) . "<br>";
+
+echo next($capitais) . "<br>";
+
+echo next($capitais) . "<br>";
+
+echo next($capitais) . "<br>";
+
+echo next($capitais) . "<br>";
+
+echo next($capitais) . "<br>";
+
+echo next($capitais) . "<br>";
+
+echo next($capitais) . "<br>";
+
+echo next($capitais) . "<br>";
+
+
+?>
