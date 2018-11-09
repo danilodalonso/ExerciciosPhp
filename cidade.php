94b6df77d1381d0f047e0de80c9be1a4edd2d3a7
Danilo Dalonso Gamboa Peres <danilodgperes@yahoo.com.br>
2018-11-09 15:44:23 -0200

    Array Associativos

---
+++ cidade.php
@@ -0,0 +1,18 @@
+<?php
+
+$cidade = array(
+
+   "nome" => "São Paulo",
+   "aniversário" => "25 de janeiro",
+   "fundação" => "25 de janeiro de 1554",
+   "gentílico" => "Paulistano",
+   "lema" => "Faça-se o melhor",
+   "padroeiro" => "Santa Ana",
+   "prefeito" => "Bruno Covas"   
+); 
+
+foreach($cidade as $cidade){
+    echo ($cidade) . "<br>";
+}
+
+?>
