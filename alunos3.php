fb164779079aaa2873d48d39140151dc55508541
Danilo Dalonso Gamboa Peres <danilodgperes@yahoo.com.br>
2018-11-10 20:37:55 -0200

    Form Escondido

---
+++ alunos3.php
@@ -0,0 +1,7 @@
+<form action="processa_form.php" method="POST"/>
+    <label>Nome</label><br>
+    <input type="text" name="nome"/><br>
+    <label>Senha</label><br>
+    <input type="password" name="senha"/><br>
+    <input type="submit" value="Enviar dados"/><br>
+</form>
---
+++ processa_form.php
@@ -0,0 +1,8 @@
+<?php
+
+echo "Os dados enviados foram: <br>";
+echo "Nome: " . $_POST["nome"] . "<br>";
+echo "Senha: " . $_POST["senha"] . "<br>";
+
+
+?>
