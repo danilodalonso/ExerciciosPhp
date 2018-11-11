26c59276ffe57a6d37ae3cdf29aa528eb5007cd2
Danilo Dalonso Gamboa Peres <danilodgperes@yahoo.com.br>
2018-11-11 17:18:25 -0200

    Formatar Tabela

---
+++ usuarios2.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Usuários</title>
+    </head>
+    <style>
+        table{
+            font-family: arial, sans-serif;
+            border-collapse: collapse;
+            width:100%;
+        }
+        th,td{
+            border: 1px solid black;
+            text-align: left;
+            padding: 8px;
+        }
+        tr:nth-child(even){
+            background-color:silver;
+        }
+
+    </style>    
+    
+    
+    <body>
+
+        <?php
+
+
+            $servidor = "cursophp_db_1";
+            $usuario = "root";
+            $senha = "phprs";
+            $banco = "curso_php";
+
+            $conn = new mysqli($servidor, $usuario, $senha, $banco);
+
+            if($conn->connect_error){
+                die("Erro ao tentar criar a conexão" . $conn->connect_error);
+            }
+
+            $sql = "SELECT * FROM `usuarios`";
+            $result = $conn->query($sql);
+
+
+            if($result->num_rows>0){
+                echo "Número de registros retornados " . $result->num_rows . "<br><br>";
+                echo "<table>";
+                echo "<tr><th>ID</th><th>E-mail</th><th>Senha</th></tr>";
+
+                while($row = $result->fetch_assoc() ){
+
+                    echo "<tr>";
+                    
+                    echo "<td>" . $row["id"] . "</td>";
+                    echo "<td>" . $row["e-mail"] . "</td>";
+                    echo "<td>" . $row["senha"] . "</td>";
+                    
+                    echo "</tr>";
+                }
+
+                echo "</table>";
+            }else{
+                echo "Ele não retornou nenhum registro";
+
+                
+            }
+
+
+            $conn -> close();
+
+
+
+        ?>
+    </body>
+</html>        
