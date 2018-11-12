4c779f39fb34d9f9805efe6334215ba7571e73db
Danilo Dalonso Gamboa Peres <danilodgperes@yahoo.com.br>
2018-11-12 13:56:10 -0200

    Formulario Adicionar

---
+++ formulario2/gravar_tarefa.php
@@ -0,0 +1,35 @@
+<?php
+
+$email = $_POST["nome"];
+$senha = $_POST["senha"];
+
+$servidor = "cursophp_db_1";
+$usuario = "root";
+$senha = "phprs";
+$banco = "curso_php";
+
+$conn = new mysqli($servidor, $usuario, $senha, $banco);
+
+if($conn->connect_error){
+    die("Erro ao tentar criar a conexão" . $conn->connect_error);
+}
+
+$sql = "INSERT INTO tarefas";
+$sql.= " (nome, senha) ";
+$sql.= " VALUES ";
+$sql.= "('".$email."',";
+$sql.= "'".$senha."',";
+$sql.= "".$status.")";
+
+if($conn->query($sql) === TRUE){
+    echo "Dados inseridos com sucesso!<br>";
+}else{
+    echo "Erro ao tentar inserir dados: " . $conn_error . "<br><br>";
+}
+
+$conn->close();
+
+
+?>
+
+<a href="usuarios.php">Voltar</a>
